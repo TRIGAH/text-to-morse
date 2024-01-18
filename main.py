3a4454c05beb8719e4bd1205c0ac1abba186d4b0
Trigah <ijenrandy2@gmail.com>
2024-01-18 02:30:35 +0100

    remove char to morse function

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
 
 # SOLUTION 1
 
-# PLAIN_TEXT = input("Enter text: ")
-# def char_to_morse(char):
-#     morse_code_dict = {
-#             'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.', 'H': '....',
-#             'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---', 'P': '.--.',
-#             'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-',
-#             'Y': '-.--', 'Z': '--..',
-#             '0': '-----', '1': '.----', '2': '..---', '3': '...--', '4': '....-', '5': '.....',
-#             '6': '-....', '7': '--...', '8': '---..', '9': '----.',
-#             ' ': '/'
-#         }
-#     return morse_code_dict.get(char.upper())
+PLAIN_TEXT = input("Enter text: ")
+def char_to_morse(char):
+    morse_code_dict = {
+            'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.', 'H': '....',
+            'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---', 'P': '.--.',
+            'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-',
+            'Y': '-.--', 'Z': '--..',
+            '0': '-----', '1': '.----', '2': '..---', '3': '...--', '4': '....-', '5': '.....',
+            '6': '-....', '7': '--...', '8': '---..', '9': '----.',
+            ' ': '/'
+        }
+    return morse_code_dict.get(char.upper())
 
-# def text_to_morse(text):
-#     print(text)
-#     print("".join( [ char_to_morse(t) for t in text ] ))
+def text_to_morse(text):
+    print(text)
+    print("".join( [ char_to_morse(t) for t in text ] ))
 
-# text_to_morse(PLAIN_TEXT)
+text_to_morse(PLAIN_TEXT)
 
 # SOLUTION 2
 
@@ -35,13 +35,9 @@ class TextMorse:
 
     def __init__(self):
         self.plain_text = input('Enter Text: ')
-        
-    def char_to_morse(self,char):
-        return self.MORSE_CODE_DICT.get(char.upper())
     
     def text_to_morse(self):
-        print("".join([ self.char_to_morse(t) for t in self.plain_text ]))
-        
+        print("".join([ self.MORSE_CODE_DICT.get(t.upper()) for t in self.plain_text ]))      
 
 text_morse = TextMorse()  
 text_morse.text_to_morse()      
